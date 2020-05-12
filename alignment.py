425e54518e5e69c2e6c6ca4233833752912cfbf8
Emil <poolsar.42@gmail.com>
2020-05-12 15:34:47 +0300

    добавили метрик, откорректировали работу алгоритмов

--- alignment.py
+++ alignment.py
@@ -17,6 +17,15 @@ def distance(a, b): #Левенштейн
 
     return current_row[n]
 
+def hamming_distance_nw(s1, s2):
+    return sum(ch1 != ch2 and ch1 != '-' and ch2 != '-' for ch1,ch2 in zip(s1,s2))
+
+def hamming_distance_fasta(s1, s2):
+    return sum(ch1 != ch2 for ch1,ch2 in zip(s1,s2))
+
+def hamming_distance_blast(s1, s2):
+    return sum(ch1 != ch2 for ch1,ch2 in zip(s1,s2))
+
 def ALIGN_NW (x, y):
     ALIGNx, ALIGNy = [], []
     N, M = len(x), len(y)
@@ -68,7 +77,7 @@ def ALIGN_NW (x, y):
     ALIGNx.append(x[i])
     ALIGNy.append(y[j])
     
-    while (i < N - 1 and j < M - 1):
+    while (i < N - 1 or j < M - 1):
         if (i < N - 1 and j < M - 1):
             MAX = max(W[i + 1][j + 1], W[i][j + 1], W[i + 1][j])
             
@@ -101,12 +110,13 @@ def ALIGN_NW (x, y):
 
     x_end, y_end = '', ''
 
-    length = len(ALIGNx) if len(ALIGNx) < len(ALIGNy) else len(ALIGNy)
+    length = max(len(ALIGNx),len(ALIGNy))
     for i in range(length):
         x_end += ALIGNx[i]
     for j in range(length):
         y_end += ALIGNy[j]
-     
+    power = max(N, M)
+
     return distance(x_end, y_end), x_end, y_end
 
 def FASTA(x, y):
@@ -134,22 +144,24 @@ def FASTA(x, y):
                     
     for i in range(1, N):
         for j in range(1, M):
-            if (A[i][j] != 0 and A[i-1][j-1] != -1 and A[i][j] != -1):
-                A[i][j] += A[i-1][j-1]
+            if (A[i][j] == 1 and A[i - 1][j - 1] != -1):
+                A[i][j] += A[i - 1][j - 1]
+                
+    for i in range(N-2, -1, -1):
+        for j in range(M-2, -1, -1):
+            if (A[i][j] >= 1 and A[i + 1][j + 1] >= 1):
+                A[i][j] = A[i + 1][j + 1]
                 
     index = []
-    k, h, start = 0, 0, 0
+    h, start_x, start_y = 0, 0, 0
     
-    for i in range(N):
-        if len(index) > 0:
-            start = index[h-1][1]
-        for j in range(start, M):
-            if A[i][j] >= 1:
-                if start > 0:
-                    if j <= index[h-1][1] or j > index[h-1][1]+1 :
-                        k += 1
-                index.append([i, j, k])
+    for i in range(start_x, N):
+        for j in range(start_y, M):
+            if A[i][j] >= 5:
+                index.append([i, j])
                 h += 1
+                start_y = index[h - 1][1] + 1
+                start_x = index[h - 1][0] + 1
                 break
 
     ALIGNx, ALIGNy = [], []
@@ -189,46 +201,71 @@ def FASTA(x, y):
 
     x_end, y_end = '', ''
 
-    length = len(ALIGNx) if len(ALIGNx) < len(ALIGNy) else len(ALIGNy)
+    length = max(len(ALIGNx),len(ALIGNy))
     for i in range(length):
         x_end += ALIGNx[i]
     for j in range(length):
         y_end += ALIGNy[j]
-                   
-    return distance(x_end, y_end), x_end, y_end
+    power = max(N, M)
+    while(len(x_end) < power):
+        x_end += '-'
+    while(len(y_end) < power):
+        y_end += '-'
+    k, l = 0, 0
+    for i in x_end:
+        if i != '-':
+            k += 1
+    for j in y_end:
+        if j != '-':
+            l += 1
+    k += 1
+    l += 1
+    while k < N:
+        x_end += str(x[k])
+        k += 1
+    while l < M:
+        y_end += str(y[l])
+        l += 1
+    if (len(x_end) < len(y_end)):
+        while(len(x_end) < len(y_end)):
+            x_end += '-'
+    if (len(y_end) < len(x_end)):
+        while(len(y_end) < len(x_end)):
+            y_end += '-'
+    return distance(x_end, y_end), x_end, y_end, k, l
 
 def BLAST (x, y, w):
     ALIGNx, ALIGNy, strings = '', '', []
     N, M, s = len(x), len(y), len(y) % w
     for i in range(M - w + 1):
         pause = ''
-        for j in range(5):
+        for j in range(w):
             pause += y[i + j]
         strings.append([pause, i, j + i])
-    i, j = 0, 0
+    i, j = 0, 0 # в строке x, в строке y
     for substr in strings:
         
-        left_end, right_end = substr[1], substr[2]
+        left_end, right_end = substr[1], substr[2] # в строке y
         substring_in_y = substr[0]
-        if substring_in_y in x and j < left_end or j == left_end == 0:
+        if x.find(substr[0]) > -1 and i <= x.find(substr[0]) and j <= left_end:
             
             init = x.find(substr[0])
-            final = init + len(substr[0]) - 1
+            final = init + len(substr[0]) - 1 # init и final в x
             substring_in_x = substr[0]
             
-            while distance(substring_in_x,substring_in_y) <= 2:
+            while hamming_distance_blast(substring_in_x, substring_in_y) <= 1:
                 
                 flag_x, flag_y = substring_in_x, substring_in_y
                 
-                if left_end > 0:
+                if left_end > j:
                     left_end -= 1
                     substring_in_y = str(y[left_end]) + str(substring_in_y)
-                    if init > 0:
+                    if init > i:
                         init -= 1
                         substring_in_x = str(x[init]) + substring_in_x
-                    if init == 0:
+                    if init == i:
                         substring_in_x = '-' + substring_in_x
-                
+            
                 if right_end < M - 1:
                     right_end += 1
                     substring_in_y = str(substring_in_y) + str(y[right_end])
@@ -243,27 +280,96 @@ def BLAST (x, y, w):
                     final += 1
                     substring_in_x += str(x[final])
                         
-                if left_end == 0 and init > 0:
+                if left_end == j and init > i:
                     init -= 1
                     substring_in_y = '-' + str(substring_in_y)
                     substring_in_x = str(x[init]) + substring_in_x
                         
                 if (flag_x == substring_in_x or flag_y == substring_in_y):
                     break
-    
+
+            if (j < left_end):
+                while (j < left_end):
+                    ALIGNy += y[j]
+                    j += 1
+            if (i < init):
+                while (i < init):
+                    ALIGNx += x[i]
+                    i += 1
+            
             ALIGNx += substring_in_x            
             ALIGNy += substring_in_y
-            j = right_end
-  #  if final < N:
-  #      for i in range(final+1, len(x)):
-  #          ALIGNx += str(x[i])
-  #          ALIGNy += '-'    
+            j = right_end + 1
+            i = final + 1
 
-    length = len(ALIGNx) if len(ALIGNx) < len(ALIGNy) else len(ALIGNy)
-    for i in range(length):
-        x_end += ALIGNx[i]
-    for j in range(length):
-        y_end += ALIGNy[j]
+    x_end, y_end = '', ''
 
-    return distance(x_end, y_end), x_end, y_end
+    for i in ALIGNx:
+        x_end += i
+    for j in ALIGNy:
+        y_end += j
+        
+    k, l = 0, 0
+    for i in x_end:
+        if i != '-':
+            k += 1
+    for j in y_end:
+        if j != '-':
+            l += 1
+    
+    if k < N:
+        while k < N:
+            x_end += str(x[k])
+            k += 1
+    if l < M:
+        while l < M:
+            y_end += str(y[l])
+            l += 1
+            
+    if (len(x_end) < len(y_end)):
+        while(len(x_end) < len(y_end)):
+            x_end += '-'
+    if (len(y_end) < len(x_end)):
+        while(len(y_end) < len(x_end)):
+            y_end += '-'
+    return al.distance(x_end, y_end), x_end, y_end, k, l
 
+def GenSearch(x):
+    start, end = [], []
+    i, j = 0, 0
+    while 1:
+        l = x.find('ATG', i, len(x) - 1)
+        if l == -1:
+            break
+        if l % 3 == 0:
+            start.append(l)
+        i = l + 1
+    while 1:
+        l = x.find('TAA', j, len(x) - 1)
+        if l != -1 and l % 3 == 0:
+            end.append(l)
+        k = x.find('TAG', j, len(x) - 1)
+        if k != -1 and k % 3 == 0:
+            end.append(k)
+        p = x.find('TGA', j, len(x) - 1)
+        if p != -1 and p % 3 == 0:
+            end.append(p)
+        j = max(p, l, k) + 1
+        if ( k == l == p == -1):
+            break
+    genes = []
+    k = 0
+    for i in start:
+        f = 0
+        if int(i) > k :
+            for j in end:
+                if int(j) > i :
+                    f = 1
+                    s = x[int(i): int(j)]
+                    k = int(j)
+                    break
+        if f == 1:
+            genes.append(s)
+    return genes
+            
+    
